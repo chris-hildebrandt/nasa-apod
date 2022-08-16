dd079182c53681c7fc636b6c8173fdc116f773d6
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-16 15:16:02 -0600

    homepage js setup

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,5 @@
 <template>
-<div>
-  
-</div>
+  <div class="home-page" :img="APOD.img"></div>
   <!-- <button @click="getSomething">Get Something</button>
   <div class="" v-for="s in something" :key="s.id"></div> -->
   <!--                        ^^^ this name (typically the plural) matches the object/array in the appstate -->
@@ -11,39 +9,44 @@
 </template>
 
 <script>
+import { computed, onMounted } from '@vue/runtime-core';
+import { logger } from '../utils/Logger.js';
 import { AppState } from "../AppState.js";
 import Pop from "../utils/Pop.js";
 
-// import appstate here
-// import computed here
-
 export default {
+  setup(){
   // private functions
-  getCurrentDay(){
-
-  },
-  
+    async function getAPOD(date){
+      try {
+      await APODService.getAPOD(date)
+      } catch (error) {
+      logger.error('[]', error)
+      Pop.error(error)
+      }
+    }
   // props go here in the component files, and are accessible even though they do not get returned
-  // eg.:
-  props: {},
 
   // lifecycle hooks go here:
   // onMounted(()=> getSomething())
-  onMounted(()=> getCurrentDay());
-  setup() {
+  onMounted(() => { 
+    getAPOD()
+  })
     // place all functions in here this is our new controller, so do all the async, trycatch stuff we usually do in the controller.
 
     // anything you want to use up in the template must be passed through here V
     return {
+      APOD: computed(() => AppState.APOD),
+      date: computed(() => AppState.date),
+      getAPOD
       // aka public, stuff the user needs
       // something: computed(() => AppState.something),
       // you can return the functions here from the private section if you want the user to access them
       // or you can write the entire function here is you don't need them accessible by hooks
       // getSomething
-    };
-  };
+    }
+  }
 }
-
 </script>
 
 <style scoped lang="scss">
