8b810e0b771b81ab0de1a44b0e83c3c272e1a377
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-16 15:19:36 -0600

    appstate up

--- src/AppState.js
+++ src/AppState.js
@@ -1,7 +1,14 @@
 import { reactive } from 'vue'
+import { APOD } from "./models/APOD.js"
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   user: {},
-  account: {}
+
+  account: {},
+
+  /**@type {import('./models/APOD.js').APOD} */
+  APOD: null,
+
+  date: new Date()
 })
