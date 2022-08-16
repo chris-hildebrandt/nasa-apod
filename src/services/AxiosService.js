1ee26ab7179491c367385cde22183d763b1f310c
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-16 14:49:01 -0600

    setup axiosService

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -4,3 +4,11 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+export const APODApi = Axios.create({
+  baseURL: 'https://api.nasa.gov/planetary/apod',
+  timeout: 4000,
+  params: {
+    api_key: 'QShafLJ9D3b3cNUdHe4dPcilGD9moeVSAQ7TLDKQ'
+  }
+})
