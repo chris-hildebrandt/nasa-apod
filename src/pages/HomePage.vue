274b425246e614cc0cd52177a477062b15b1713a
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-16 14:58:59 -0600

    model up

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,13 @@
 <template>
-  <button @click="getSomething">Get Something</button>
-  <div class="" v-for="s in something" :key="s.id"></div>
+<div>
+  
+</div>
+  <!-- <button @click="getSomething">Get Something</button>
+  <div class="" v-for="s in something" :key="s.id"></div> -->
   <!--                        ^^^ this name (typically the plural) matches the object/array in the appstate -->
   <!-- the above div passes the data to the component below  -->
   <!--               VVV  this name reflects the name of the class in the props of the component -->
-  <SomethingCard :something="s" />
+  <!-- <SomethingCard :something="s" /> -->
 </template>
 
 <script>
@@ -14,41 +17,35 @@ import Pop from "../utils/Pop.js";
 // import appstate here
 // import computed here
 
-// export default {
+export default {
   // private functions
-        // async function getSomething() {
-        // try {
-        //   await somethingService.getSomething;
-        // } catch (error) {
-        //   logger.error("[getting items]", error);
-        //   Pop.error(error);
-        // }
-        // }
+  getCurrentDay(){
+
+  },
+  
   // props go here in the component files, and are accessible even though they do not get returned
   // eg.:
-  // props: {
-  //   something: {}
-  // },
+  props: {},
 
   // lifecycle hooks go here:
   // onMounted(()=> getSomething())
-  // setup() {
+  onMounted(()=> getCurrentDay());
+  setup() {
     // place all functions in here this is our new controller, so do all the async, trycatch stuff we usually do in the controller.
 
     // anything you want to use up in the template must be passed through here V
-    // return {
+    return {
       // aka public, stuff the user needs
       // something: computed(() => AppState.something),
       // you can return the functions here from the private section if you want the user to access them
       // or you can write the entire function here is you don't need them accessible by hooks
       // getSomething
-  //     },
-  //   };
-  // },
+    };
+  };
+}
+
 </script>
 
-//        V   this stuff needs to go away when rewriting the homepage layout
-
 <style scoped lang="scss">
-
+//        V   this stuff needs to go away when rewriting the homepage layout
 </style>
